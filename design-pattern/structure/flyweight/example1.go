7b3b85e3cdd838819975da4773bf422ffe4c9505
Jason <jason.yien@outlook.com>
2024-06-18 18:00:10 +0800

    flyweight

--- design-pattern/structure/flyweight/example1.go
+++ design-pattern/structure/flyweight/example1.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
 type Shape interface {
 	getName() string
 	setName(name string)
@@ -43,11 +49,60 @@ func (c *Circle) getY() float64 {
 func (c *Circle) setY(y float64) {
 	c.y = y
 }
-
 func (c *Circle) setRadius(radius float64) {
 	c.radius = radius
 }
-
 func (c *Circle) getRadius() float64 {
 	return c.radius
+}
+
+func (c *Circle) show() {
+	fmt.Println(c.name,
+		", color=", c.color,
+		", x=", c.x,
+		", y=", c.y,
+		", radius=", c.radius)
+}
+
+// 包级别变量，类似静态变量
+var colorShape = map[string]Shape{}
+
+// 包级别方法，类似静态方法
+func getCircle(color string) Shape {
+	if _, ok := colorShape[color]; !ok {
+		circle := &Circle{}
+		circle.setName("圆形")
+		circle.setColor(color)
+		colorShape[color] = circle
+	}
+	return colorShape[color]
+}
+
+const l int = 5
+
+var colors [l]string = [l]string{"red", "blue", "yellow", "black", "green"}
+
+func randomColor() string {
+	rand.Seed(time.Now().UnixNano()) //一次调用，到处生效
+	idx := rand.Intn(l)
+	return colors[idx]
+}
+
+func randomXY() float64 {
+	return rand.Float64() * 100
+}
+
+func main() {
+	//创建大量对象
+	for i := 0; i < 1000; i++ {
+		circle := getCircle(randomColor()).(*Circle) //类型断言为Circle
+		circle.setX(randomXY())
+		circle.setY(randomXY())
+		circle.setRadius(10.2)
+	}
+
+	fmt.Println(len(colorShape))
+
+	circle := getCircle(randomColor()).(*Circle)
+	circle.show()
 }
--- go-basic/random/random.go
+++ go-basic/random/random.go
@@ -3,6 +3,7 @@ package random
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
 // 未设定种子数而产生的随机数是固定数
@@ -17,7 +18,8 @@ func testRandom1() {
 }
 
 func testRandom2() {
-	rand.Seed(100) // ！！！设置1次到处生效，全局变量
+	//一般设置为当前时间
+	rand.Seed(time.Now().UnixNano()) // ！！！设置1次到处生效，全局变量
 	r := rand.Int()
 	fmt.Println(r) // 7530908113823513298
 }
